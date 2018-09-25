e04ae92c9b10c94ed8bd38f7a9b3d0f79685c10f
Alana Gudinas <31524138+alanagudinas@users.noreply.github.com>
2018-09-25 09:27:51 -0400

    Add files via upload

--- GlobalVar.m
+++ GlobalVar.m
@@ -4,6 +4,17 @@ global hMethod
 global output_graph
 global help_dlg
 
+global verbose
+global show_results
+global show_progress
+
 hMethod = 1; 
 output_graph = 1; % set to 0 to repress graphical outputs 
 help_dlg = 0; % set to 1 if you want help boxes to pop up
+
+% the following are variables from the ACO program. They are set to zero to
+% suppress graphical and command line outputs. 
+verbose = 0;
+show_results = 0;
+show_progress = 0;
+
